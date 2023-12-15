65ef62fc444ae602d2f838dd2389282b5a5c51d7
Oleksandr Aleksieiev <frontdev1991@gmail.com>
2023-12-15 17:49:36 +0100

    small change

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,7 +3,7 @@ import React, {FC, ReactElement} from 'react';
 const AppHeader: FC = (): ReactElement => {
   return (
     <header className={'w-full h-20 flex flex-row items-center justify-between overflow-hidden border-b shadow-md px-4 md:px-8'}>
-      <div className={'h-full'}>
+      <div className={'h-full'} >
         Print speed
       </div>
 
