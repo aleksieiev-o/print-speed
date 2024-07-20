77f73e91c1fbfd9d95520148045aebaa08d767b6
Oleksandr Aleksieiev <frontdev1991@gmail.com>
2024-07-20 18:31:14 +0200

    edit: home screen styles edited

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -14,28 +14,26 @@ const Home: FC = observer((): ReactElement => {
 
   return (
     <AppWrapper>
-      <div className={'w-full h-full grid grid-cols-1 justify-items-center content-center'}>
-        <Card className={'shadow-md'}>
-          <CardHeader className={'justify-center'}>
-            <CardTitle className={'text-center'}>Game rule</CardTitle>
-          </CardHeader>
+      <Card className={'w-full h-full flex flex-col items-center justify-center overflow-hidden shadow-md'}>
+        <CardHeader className={'justify-center'}>
+          <CardTitle className={'text-center'}>Game rule</CardTitle>
+        </CardHeader>
 
-          <CardContent className={'justify-center'}>
-            <p className={'text-center'}>The goal of the game is to type symbols, as quickly as possible before the time runs out</p>
-          </CardContent>
+        <CardContent className={'justify-center !pt-0'}>
+          <p className={'text-center'}>The goal of the game is to type symbols, as quickly as possible before the time runs out</p>
+        </CardContent>
 
-          <CardFooter className={'justify-center'}>
-            {IsNotDesktop ? (
-              <p className={'text-center font-bold text-red-500'}>You can use this application only on a personal computer</p>
-            ) : (
-              <Button onClick={() => navigate(ERouter.GAME)} variant={'default'} title={'Go to the game'}>
-                <SquarePlay className="mr-4 h-5 w-5" />
-                <p>Go to the game</p>
-              </Button>
-            )}
-          </CardFooter>
-        </Card>
-      </div>
+        <CardFooter className={'justify-center'}>
+          {IsNotDesktop ? (
+            <p className={'text-center font-bold text-red-500'}>You can use this application only on a personal computer</p>
+          ) : (
+            <Button onClick={() => navigate(ERouter.GAME)} variant={'default'} title={'Go to the game'}>
+              <SquarePlay className="mr-4 h-5 w-5" />
+              <span>Go to the game</span>
+            </Button>
+          )}
+        </CardFooter>
+      </Card>
     </AppWrapper>
   );
 });
